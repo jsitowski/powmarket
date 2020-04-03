70991150e62bd931ac7571f1d5a943299e67e693
synfonaut <synfonaut@protonmail.com>
2020-04-02 18:34:42 -0700

    disable content type

--- src/data.js
+++ src/data.js
@@ -76,12 +76,14 @@ export async function processDisplayForMagicNumber(tx={}) {
         tx.display_power = processDisplayForPower(tx.power);
     }
 
+    /*
     if (tx.content_type) {
         if (tx.content_type === "bitsv") {
             tx.display_network = "Bit.sv";
             tx.display_link = "https://bit.sv";
         }
     }
+    */
 
     // mustache scoping bugs
     if (!tx.emoji) { tx.emoji = null }
